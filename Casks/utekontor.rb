f00f4cc838289ee408a8450c57ae662d3ccdf0d6
Sten <sten@Sten-sin-MacBook-Pro.local>
2026-06-05 09:47:58 +0200

    chore(cask): bump to 0.1.3

--- Casks/utekontor.rb
+++ Casks/utekontor.rb
@@ -1,8 +1,8 @@
 # Homebrew cask for Utekontor. This repo is also a tap: see README.
 # When cutting a new release, update version and sha256 to match the uploaded zip.
 cask "utekontor" do
-  version "0.1.2"
-  sha256 "605cd27a08bd1ecb6fe31fd25bac2203e950170b52fcff1b7a3c2c40abe7e6fe"
+  version "0.1.3"
+  sha256 "398923f399d12f6b9495819b56cc53b662d522a36cc0294bd09c70de8a2c2777"
 
   url "https://github.com/JorgenStensrud/utekontor-mac/releases/download/v#{version}/Utekontor-#{version}.zip"
   name "Utekontor"
